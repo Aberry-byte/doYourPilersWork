3c094ec891de5e382a42b5c89d83c2b2e0464f8f
Alex Huft <alexhuft@gmail.com>
2022-06-23 00:27:21 -0700

    can now check if pilers exist

--- do_your_pilers_work.py
+++ do_your_pilers_work.py
@@ -4,15 +4,20 @@
 import shutil
 
 
-compilers = ["gcc", "g++", "java","go", "rustc", "node", "tsc"]
-cmd_exists = [False, False, False, False, False, False, False]
+compilers = ["gcc", "g++", "python3", "java", "go", "rustc", "node", "tsc", "lua", "ruby"]
+cmd_exists = [False, False, False, False, False, False, False, False, False, False]
 
 
 def command_exists(cmd: str) -> bool:
-    if len(shutil.which(cmd)) > 0:
+    if shutil.which(cmd) is not None:
         return True
     return False
 
 
 if __name__ == "__main__":
-
+    for index, compiler in enumerate(compilers):
+        if command_exists(compiler):
+            cmd_exists[index] = True
+            print(f"{compiler:<12}found")
+        else:
+            print(f"{compiler:<12}not found")
