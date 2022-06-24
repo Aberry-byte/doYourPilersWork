3732990ac92872e808ef173e6b8ddf53a342a708
Alex H <alexhuft@gmail.com>
2022-06-24 12:50:51 -0700

    added in list of test files

--- do_your_pilers_work.py
+++ do_your_pilers_work.py
@@ -6,6 +6,8 @@ import shutil
 
 compilers = ["gcc", "g++", "python3", "java", "go", "rustc", "node", "tsc", "lua", "ruby"]
 cmd_exists = [False, False, False, False, False, False, False, False, False, False]
+test_files = ["test.c", "test.cpp", "test.py", "test.java", "test.go", "test.rs", "test.js", "testts.ts", "test.lua",
+              "test.rb"]
 
 
 def command_exists(cmd: str) -> bool:
