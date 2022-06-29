825cf6596cbffbec0212816636f1c63faefab558
Alex H <alexhuft@gmail.com>
2022-06-29 01:14:15 -0700

    changed to using a dictionary and updated how info is displayed

--- do_your_pilers_work.py
+++ do_your_pilers_work.py
@@ -5,8 +5,29 @@ import shutil
 import subprocess
 
 
-compilers = ["gcc", "g++", "python3", "javac", "go", "rustc", "node", "tsc", "lua", "ruby"]
-cmd_exists = [False, False, False, False, False, False, False, False, False, False]
+class Colors:
+    black = "\u001b[30m"
+    red = "\u001b[31m"
+    green = "\u001b[32m"
+    yellow = "\u001b[33m"
+    blue = "\u001b[34m"
+    magenta = "\u001b[35m"
+    cyan = "\u001b[36m"
+    white = "\u001b[37m"
+    reset = "\u001b[0m"
+
+
+compilers = {"gcc": False,
+             "g++": False,
+             "python3": False,
+             "javac": False,
+             "go": False,
+             "rustc": False,
+             "node": False,
+             "tsc": False,
+             "lua": False,
+             "ruby": False}
+
 test_files = ["test.c", "test.cpp", "test.py", "test.java", "test.go", "test.rs", "test.js", "testts.ts", "test.lua",
               "test.rb"]
 
@@ -24,20 +45,28 @@ if __name__ == "__main__":
         print("Please install 'make' through your package manager")
 
     # if make is installed we can continue with the program
-    for index, compiler in enumerate(compilers):
-        if command_exists(compiler):
-            cmd_exists[index] = True
-            print(f"{compiler:<12}found")
+    # This first loop checks if the piler exists and if it
+    #     does then we can set the value to True
+    for piler, exists in compilers.items():
+        if command_exists(piler):
+            compilers[piler] = True
+            print(f"{piler:<12}{Colors.green}found{Colors.reset}")
         else:
-            print(f"{compiler:<12}not found")
+            print(f"{piler:<12}{Colors.red}not found{Colors.reset}")
 
+    # new line
     print()
 
-    for index, value in enumerate(compilers):
-        if cmd_exists[index]:
-            print(f"**** testing {value} ****")
-            p = subprocess.run(f"make {value}", shell=True, check=True)
-            print(f"{value} works correctly")
+    # Now we can go over the pilers again and if it exists
+    #     Then we can use the test files to check that
+    #     it is working
+    for piler, exists in compilers.items():
+        if exists:
+            p = subprocess.run(f"make {piler}", shell=True)
+            if p.returncode == 0:
+                print(f"{piler:<12}{Colors.green}working{Colors.reset}")
+            else:
+                print(f"{piler:<12}{Colors.red}not working{Colors.reset}")
 
-    print("\nClearing binaries")
+    print("\nClearing test files")
     subprocess.run(f"make clean", shell=True)
